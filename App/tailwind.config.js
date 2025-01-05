e4315e6d5ca57b8c615ec4b57ae1a8f5ca871e0d
Labancz Tamás <labancztamas@yahoo.hu>
2025-01-05 02:34:42 +0200

    Kanban 0.1

--- App/tailwind.config.js
+++ App/tailwind.config.js
@@ -18,14 +18,17 @@ export default {
         'h-md': { 'raw': '(min-height: 641px) and (max-height: 1024px)' }, // Between 641px and 1024px
         'h-lg': { 'raw': '(min-height: 1025px)' }, // Screen height >= 1025px
       },
+     
       colors: {
         light: {
           bg: '#ffffff', // Light mode background
           text: '#000000', // Light mode text
+          backgroundImage: 'linear-gradient(to bottom, #EFEFEF 0%, #CDCDCD 78%, #FCFCFD 100%)',
         },
         dark: {
           bg: '#121212', // Dark mode background
           text: '#ffffff', // Dark mode text
+          backgroundImage: 'linear-gradient(to bottom, rgb(206, 13, 13) 0%, rgb(173, 25, 25) 78%, rgb(18, 18, 194) 100%)',
         },
       },
     },
