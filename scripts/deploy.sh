ca0e6eeabeda70c623beee41cb15a951ec0a2a37
Labancz Tamás <tamas.labancz@nixs.com>
2026-04-09 17:08:51 +0200

    chor: github deploy action

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+APP_DIR="/opt/anl"
+COMPOSE_DIR="/opt/anl/backend"
+
+echo "🚀 Deploying..."
+
+cd $APP_DIR
+git pull origin main
+
+cd $COMPOSE_DIR
+docker compose down
+docker compose up -d --build
+
+echo "✅ Done!"
